91088e6da8c0602f2c384375762d6cc7babedd24
srajat <rajat.shrivastava.30@gmail.com>
2016-04-20 23:12:06 -0400

    paint fence solution

--- src/DynamicProgramming/PaintFence.java
+++ src/DynamicProgramming/PaintFence.java
@@ -6,10 +6,17 @@ package DynamicProgramming;
 public class PaintFence {
 
     public int numWays(int n, int k) {
-        return 0;
+        if(n<2 || k==0) return n*k;
+        int same = k, diff = k*(k-1);
+        for(int i=2;i<n;++i){
+            int temp = diff;
+            diff = (same+diff)*(k-1);
+            same=temp;
+        }
+        return same+diff;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(new PaintFence().numWays(6,2));
     }
 }
