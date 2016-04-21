368f248eb4ef2de543a458c537f3361525b5b17f
srajat <rajat.shrivastava.30@gmail.com>
2016-04-20 23:29:36 -0400

    house robber solution

---
+++ src/DynamicProgramming/HouseRobber.java
@@ -0,0 +1,23 @@
+package DynamicProgramming;
+
+/**
+ * Created by rajat on 4/20/16.
+ */
+public class HouseRobber {
+
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length < 2) return nums[0];
+        int[] dp = new int[nums.length + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 2; i <= nums.length; ++i) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+        }
+        return dp[nums.length];
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
