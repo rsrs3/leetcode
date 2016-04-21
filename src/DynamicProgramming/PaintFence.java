4444169a77e970782b14d38373eac67076b21b38
srajat <rajat.shrivastava.30@gmail.com>
2016-04-20 22:59:07 -0400

    first commit

---
+++ src/DynamicProgramming/PaintFence.java
@@ -0,0 +1,15 @@
+package DynamicProgramming;
+
+/**
+ * Created by rajat on 4/20/16.
+ */
+public class PaintFence {
+
+    public int numWays(int n, int k) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
